838f65d32fec7a9d5f531cf3d4da09f0e4e3eafe
biancapcr <biancacristina.pocora@gmail.com>
2025-12-15 15:32:40 +0100

    [feat] bonus 1: handle empty api results in getdashboardata function

--- Promises/promises.js
+++ Promises/promises.js
@@ -23,18 +23,20 @@ async function getDashboardData(query) {
       airportsFetch,
     ]);
 
-    // primo elemento da ciascun array
-    const destination = destinations[0];
-    const weather = weathers[0];
-    const airport = airports[0];
+    // primo elemento da ciascun array (se esiste)
+    // se l'array è vuoto -> {}
+    const destination = destinations[0] || {};
+    const weather = weathers[0] || {};
+    const airport = airports[0] || {};
 
     // oggetto finale con i dati aggregati
+    // se un dato manca -> null
     const data = {
-      city: destination.name, // nome città
-      country: destination.country, // paese
-      temperature: weather.temperature, // temperatura attuale
-      weather: weather.weather_description, // descrizione meteo
-      airport: airport.name, // aeroporto principale
+      city: destination.name || null, // nome città
+      country: destination.country || null, // paese
+      temperature: weather.temperature || null, // temperatura attuale
+      weather: weather.weather_description || null, // descrizione meteo
+      airport: airport.name || null, // aeroporto principale
     };
 
     // restituzione dell’oggetto
@@ -46,16 +48,22 @@ async function getDashboardData(query) {
 }
 
 // test
-getDashboardData("london")
+getDashboardData("vienna")
   .then((data) => {
     // stampa in console l'oggetto ricevuto
     console.log("Dashboard data:", data);
 
     // messaggio formattato
-    console.log(
-      `${data.city} is in ${data.country}.\n` +
-        `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
-        `The main airport is ${data.airport}.\n`
-    );
+    let message = "";
+
+    if (data.city && data.country)
+      message += `${data.city} is in ${data.country}.\n`;
+
+    if (data.temperature && data.weather)
+      message += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`;
+
+    if (data.airport) message += `The main airport is ${data.airport}.\n`;
+
+    console.log(message || "No data available for this city.");
   })
   .catch((error) => console.error(error.message));
